25114373b7f7ba56d1ebd364e3ad6e4c7a520598
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-08-17 16:47:24 +0300

    Task 16. Users Feature Area

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -6,10 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { UserComponent, UserListComponent, UserFormComponent } from './components';
 
 import { UsersRoutingModule } from './users-routing.module';
+import {UsersComponent} from "./users.component";
 
 
 @NgModule({
-  declarations: [UserComponent, UserListComponent, UserFormComponent],
+  declarations: [UserComponent, UserListComponent, UserFormComponent, UsersComponent],
   imports: [
     CommonModule,
     FormsModule,
