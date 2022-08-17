8ad713a501d7a9cd1c9cdef453c04c7766f5d76a
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-08-17 16:11:59 +0300

    Task 09. Tasks Feature Route Configuration

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TaskListComponent } from './components';
+
+const routes: Routes = [
+  {
+    path: 'task-list',
+    component: TaskListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
