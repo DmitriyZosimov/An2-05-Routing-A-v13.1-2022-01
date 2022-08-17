e8a25f83ae2b3815642f238ea7d5ab4673f99dba
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-08-17 16:00:05 +0300

    Task 04. Import Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
+import {Router} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+
+import {AppComponent} from './app.component';
+import {LayoutModule} from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -12,12 +15,19 @@ import { LayoutModule } from './layout/layout.module';
   imports: [
     BrowserModule,
     FormsModule,
-    LayoutModule
+    LayoutModule,
+    // MUST BE LAST
+    AppRoutingModule
   ],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+
+  constructor(router: Router) {
+    const replacer = (key: string, value: any): string =>
+      typeof value === 'function' ? value.name : value;
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
 
 }
